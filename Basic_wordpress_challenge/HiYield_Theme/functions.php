f06326d3ad8fef4be193ccf34a6d1a1f9aa710a9
loganfordwd <108948686+loganfordwd@users.noreply.github.com>
2022-07-11 14:59:28 +0100

    Add files via upload

---
+++ Basic_wordpress_challenge/HiYield_Theme/functions.php
@@ -0,0 +1,14 @@
+<?php 
+
+function hi_yield_register_styles(){
+    
+    $version = wp_get_theme()->get('Version');
+    wp_enqueue_style('hi-yield-bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css". "/style.css", array(), '4.4.1', 'all');
+    wp_enqueue_style('hi-yield-bootstrap', "https://cdnjs.cloudfare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css". "/style.css", array(), '5.13.0', 'all');
+
+}
+
+add_action('wp_enqueue_scripts', 'hi_yield_register_styles')
+
+
+?>
